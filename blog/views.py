9b836a2555c7fb799f87df40daa96e7cc5f35c55
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-08-30 19:34:02 -0500

    dummy posts added to blog/views and displayed on templates/home.html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'Krisztian Markella',
+        'title': 'Blog Post 1',
+        'content': "First Blog Post",
+        'date_posted': 'August 28, 2023',
+    },
+
+    {
+        'author': 'Christian Markella',
+        'title': 'Blog Post 2',
+        'content': "Second Blog Post",
+        'date_posted': 'August 29, 2023',
+    },
+    
+    {
+        'author': 'Mark Markella',
+        'title': 'Blog Post 3',
+        'content': "Third Blog Post",
+        'date_posted': 'August 30, 2023',
+    },
+]
+
 def home(request):
-    return render(request=request, template_name='blog/home.html')
+    context = {
+        'posts': posts,
+    }
+    return render(request=request, template_name='blog/home.html', context=context)
 
 def about(request):
     return render(request=request, template_name='blog/about.html')

65b202a48aa1a7f807f5e9757de3679a26d98221
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-09-05 23:20:15 -0500

    Added PostDetailView class to view individual posts one by one.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from . import views
 
 
 urlpatterns = [
-    # path(route='', view=views.home, name='blog-home'),
     path(route='', view=PostListView.as_view(), name='blog-home'),
+    path(route='post/<int:pk>', view=PostDetailView.as_view(), name='post-detail'),
     path(route='about/', view=views.about, name='blog-about'),
 ]

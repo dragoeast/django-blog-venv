025423d514a788e86ee6a5d4708079d5db75fd1f
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-09-06 19:12:28 -0500

    UserPostListView class and user_posts.html added to enable to view a specific user's posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,12 +5,14 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView,
 )
 from . import views
 
 
 urlpatterns = [
     path(route='', view=PostListView.as_view(), name='blog-home'),
+    path(route='user/<str:username>', view=UserPostListView.as_view(), name='user-posts'),
     path(route='post/<int:pk>', view=PostDetailView.as_view(), name='post-detail'),
     path(route='post/new', view=PostCreateView.as_view(), name='post-create'),
     path(route='post/<int:pk>/update/', view=PostUpdateView.as_view(), name='post-update'),
